3a19e555e6d391d7d2b0db5db3f8ac0a5762cc2f
mysterix <oyewoleibkay111@gmail.com>
2022-06-25 19:20:28 +0100

    zuri second task on javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+const opr = prompt("operator:+-*/")
+const num1 = parseInt(prompt("number 1: "))
+const num2 = parseInt(prompt("number 2: "))
+
+if( opr == "+")
+{
+    alert(num1 + num2)
+} else if(opr =="-")
+{
+    alert(num1-num2)
+}else if(opr == "*")
+{
+    alert( num1 * num2)
+}else if(opr == "/")
+
+{
+    alert(num1 / num2)
+}
